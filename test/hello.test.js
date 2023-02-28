b5b96cb39853df209166d2f86f08250746f12d7d
indra appkey <indra.appkey@gmail.com>
2023-02-28 10:48:39 +0800

    try unit test for response

--- test/hello.test.js
+++ test/hello.test.js
@@ -20,6 +20,30 @@ app.get('/reqTest2',(req,res)=>{
     })
 })
 
+app.get('/getParam',(req,res)=>{
+    res.send(`hello ${req.query.first_name} ${req.query.last_name}`)
+})
+
+app.get('/getQueryHeader',(req,res)=>{
+    const header = req.get('Accept')
+    res.send(`header: ${header}`)
+})
+
+app.get('/status',(req,res)=>{
+    if(req.query.name){
+        res.status(200).send(`hello ${req.query.name}`)
+    }else{
+        res.status(404).end()
+    }
+})
+
+app.get('/setResponseHeader',(req,res)=>{
+    res.set({
+        'X-Powered-By':'New SE',
+        'X-Author':'Kira'
+    }).end()
+})
+
 test("Test Hello function",async ()=>{
     const response = await request(app).get('/')
     expect(response.text).toBe('hello')
@@ -38,4 +62,32 @@ test("Test Others Request",async ()=>{
         protocol:'http',
         hostname:'127.0.0.1',
     })
+})
+
+test("Test Get Query Param",async ()=>{
+    const response=await request(app).get('/getParam').query({first_name:'Marlowe',last_name:'kira'})
+    expect(response.text).toBe('hello Marlowe kira')
+})
+
+test("Test Get Query Header",async ()=>{
+    const response= await request(app).get('/getQueryHeader').set('Accept','text/html')
+    expect(response.text).toBe('header: text/html')
+})
+
+describe("Test Response Status",()=>{
+    test("Status 200", async ()=>{
+        const response = await request(app).get('/status').query({name:'kira'})
+        expect(response.status).toBe(200)
+    })
+
+    test("Status 404", async ()=>{
+        const response = await request(app).get('/status')
+        expect(response.status).toBe(404)
+    })
+})
+
+test("Test Set Response Header",async ()=>{
+    const response = await request(app).get('/setResponseHeader')
+    expect(response.get('X-Powered-By')).toEqual('New SE')
+    expect(response.get('X-Author')).toEqual('Kira')
 })
